134fcc9a33dba6904814ce94041c4863c5372b5d
Nicolaes Tollenaar <nico.tollenaar@gmail.com>
2022-02-01 20:10:17 +0100

    done

--- app.js
+++ app.js
@@ -4,18 +4,22 @@ const PORT = 1234;
 
 app.use(express.static("public"));
 
-app.get("home", (req, res)=> {
+app.get("/home", (req, res)=> {
     res.sendFile(__dirname + "/views/home.html");
 });
 
-app.get("", (req, res)=> {
+app.get("/", (req, res)=> {
+    res.status(404).redirect("/home");
+});
+
+app.get("/about", (req, res)=> {
     res.sendFile(__dirname + "/views/about.html");
 });
 
-app.get("", (req, res)=> {
+app.get("/works", (req, res)=> {
     res.sendFile(__dirname + "/views/works.html");
 });
 
 app.listen(PORT, ()=>{
     console.log(`Server running, listening on ${PORT}`);
-})
+});
